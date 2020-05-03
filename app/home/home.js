4b19da162659cde6f71a57cefe8f5688e42bb088
DownGit <64679410+DownGit@users.noreply.github.com>
2020-05-03 17:39:16 +0600

    [feature-impl] change downloadUrlPrefix to 'https://downgit.github.io/'

--- app/home/home.js
+++ app/home/home.js
@@ -32,7 +32,7 @@ homeModule.config([
 
                     var templateUrl = "https?://github.com/.+/.+";
                     var downloadUrlInfix = "#/home?url=";
-                    var downloadUrlPrefix = "https://minhaskamal.github.io/DownGit/"+downloadUrlInfix;
+                    var downloadUrlPrefix = "https://downgit.github.io/"+downloadUrlInfix;
 
                     if ($routeParams.url) {
                         $scope.url = $routeParams.url;
